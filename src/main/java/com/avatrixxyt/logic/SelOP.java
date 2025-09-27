00c7a0670a969f7b121b6d10eb5e40e70387b4a7
AvatrixXYT <andresvillare10@gmail.com>
2025-09-26 22:39:55 -0600

    Termino de aplicasion v1.0

--- src/main/java/com/avatrixxyt/logic/SelOP.java
+++ src/main/java/com/avatrixxyt/logic/SelOP.java
@@ -1,30 +1,47 @@
 package com.avatrixxyt.logic;
 
 public class SelOP {
+    public int RespuestaCO;
 	public String Seleccion(int Dif) {
+		int num1, num2, pot;
 		GenNum gn = new GenNum();
 		Operaciones op = new Operaciones();
 		SetDif sd = new SetDif();
+		num1 = gn.Num(sd.DifNum(Dif));
+		num2 = gn.Num2(sd.DifNum(Dif));
+		pot = gn.Pontencia();
 		int operacion = gn.OP(sd.DifOP(Dif));
 		String resultado;
 		switch (operacion) {
 			case 0:
-				resultado = op.Suma(gn.Num(sd.DifNum(Dif)), gn.Num2(sd.DifNum(Dif)));
+				resultado = op.Suma(num1, num2);
+                RespuestaCO = num1 + num2;
+                System.out.println(RespuestaCO);
 				break;
 			case 1:
-				resultado = op.Resta(gn.Num(sd.DifNum(Dif)), gn.Num2(sd.DifNum(Dif)));
+				resultado = op.Resta(num1, num2);
+                RespuestaCO = num1 - num2;
+                System.out.println(RespuestaCO);
 				break;
 			case 2:
-				resultado = op.Multi(gn.Num(sd.DifNum(Dif)), gn.Num2(sd.DifNum(Dif)));
+				resultado = op.Multi(num1, num2);
+                RespuestaCO = num1 * num2;
+                System.out.println(RespuestaCO);
 				break;
 			case 3:
-				resultado = op.Div(gn.Num(sd.DifNum(Dif)), gn.Num2(sd.DifNum(Dif)));
+				resultado = op.Div(num1, num2);
+                RespuestaCO = num1 / num2;
+                System.out.println(RespuestaCO);
 				break;
 			case 4:
-				resultado = op.Raiz(gn.Num2(sd.DifNum(Dif)));
+				resultado = op.Raiz(num1);
+                RespuestaCO = (int) Math.sqrt(num1);
+                System.out.println(RespuestaCO);
 				break;
 			case 5:
-				resultado = op.Potencia(gn.Num(sd.DifNum(Dif)), gn.Pontencia());
+				resultado = op.Potencia(num1, pot);
+                RespuestaCO = (int) Math.pow(num1, pot);
+                System.out.println(RespuestaCO);
 				break;
 			default:
 				resultado = "Operacion no valida";
@@ -32,8 +49,4 @@ public class SelOP {
 		}
 		return resultado;
 	}
-	
-	public int RespuestaCo(int Dif){
-        return 0;
-    }
 }
--- src/main/java/com/avatrixxyt/swing/Jugando.java
+++ src/main/java/com/avatrixxyt/swing/Jugando.java
@@ -4,6 +4,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 
 import com.avatrixxyt.logic.*;
+
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
@@ -11,6 +12,8 @@ import java.awt.Font;
 import javax.swing.SpringLayout;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.DocumentFilter;
 
 public class Jugando {
 	
@@ -18,46 +21,97 @@ public class Jugando {
 	SelOP ops = new SelOP();
 	
 	ButtonGroup group = new ButtonGroup();
-	JFrame frmHola = new JFrame();
+	JFrame frame = new JFrame();
 	private JTextField txtRespuesta;
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	public void jugando() {
-		frmHola.setTitle("Yenga Matematico "+dif.DifText(Inicio.Dificultad));
-		frmHola.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmHola.setSize(584, 242);
+		frame.getContentPane().removeAll();
+		frame.repaint();
+		frame.setTitle("Yenga Matematico - "+dif.DifText(Inicio.Dificultad));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(584, 242);
 		SpringLayout springLayout = new SpringLayout();
-		frmHola.getContentPane().setLayout(springLayout);
+		frame.getContentPane().setLayout(springLayout);
 		
 		JLabel lblNewLabel = new JLabel(ops.Seleccion(Inicio.Dificultad));
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 0, SpringLayout.WEST, frmHola.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 584, SpringLayout.WEST, frmHola.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 0, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 584, SpringLayout.WEST, frame.getContentPane());
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		frmHola.getContentPane().add(lblNewLabel);
+		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contesta la operacion");
 		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 29, SpringLayout.SOUTH, lblNewLabel_1);
-		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 10, SpringLayout.NORTH, frmHola.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 203, SpringLayout.WEST, frmHola.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 10, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 203, SpringLayout.WEST, frame.getContentPane());
 		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		frmHola.getContentPane().add(lblNewLabel_1);
+		frame.getContentPane().add(lblNewLabel_1);
 		
 		txtRespuesta = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, txtRespuesta, 10, SpringLayout.WEST, frmHola.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, txtRespuesta, -12, SpringLayout.SOUTH, frmHola.getContentPane());
-		frmHola.getContentPane().add(txtRespuesta);
+		springLayout.putConstraint(SpringLayout.WEST, txtRespuesta, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, txtRespuesta, -12, SpringLayout.SOUTH, frame.getContentPane());
+		frame.getContentPane().add(txtRespuesta);
 		txtRespuesta.setColumns(10);
 		
 		JButton Aceptar = new JButton("Aceptar");
-		springLayout.putConstraint(SpringLayout.SOUTH, Aceptar, -9, SpringLayout.SOUTH, frmHola.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, Aceptar, -9, SpringLayout.SOUTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, txtRespuesta, -6, SpringLayout.WEST, Aceptar);
-		springLayout.putConstraint(SpringLayout.EAST, Aceptar, -10, SpringLayout.EAST, frmHola.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, Aceptar, -10, SpringLayout.EAST, frame.getContentPane());
+
+		// Agregar ActionListener para capturar el texto al presionar el botón
+		Aceptar.addActionListener(e -> {
+			int respuestaUsuario = 0;
+			try {
+				respuestaUsuario = Integer.parseInt(txtRespuesta.getText());
+				Check(respuestaUsuario);
+				// Aquí puedes usar la variable respuestaUsuario como desees
+			} catch (NumberFormatException ex) {
+				// Manejar el caso en que el campo esté vacío o no sea un número válido
+				System.out.println("Por favor ingresa un número válido.");
+			}
+		});
+
+		frame.getContentPane().add(Aceptar);
 		
+		JButton Salir = new JButton("Salir");
+		springLayout.putConstraint(SpringLayout.NORTH, Salir, 0, SpringLayout.NORTH, lblNewLabel_1);
+		springLayout.putConstraint(SpringLayout.WEST, Salir, 0, SpringLayout.WEST, txtRespuesta);
+		frame.getContentPane().add(Salir);
 		
-		frmHola.getContentPane().add(Aceptar);
-		frmHola.setVisible(true);
+		Salir.addActionListener(e -> {frame.dispose();Inicio.inicio();});
 		
+		// Crear un DocumentFilter para permitir solo números (ahora también negativos)
+		DocumentFilter soloNumerosFilter = new DocumentFilter() {
+			@Override
+			public void insertString(FilterBypass fb, int offset, String string, javax.swing.text.AttributeSet attr) throws javax.swing.text.BadLocationException {
+				StringBuilder sb = new StringBuilder(txtRespuesta.getText());
+				sb.insert(offset, string);
+				if (sb.toString().matches("-?\\d*")) {
+					super.insertString(fb, offset, string, attr);
+				}
+			}
+			@Override
+			public void replace(FilterBypass fb, int offset, int length, String text, javax.swing.text.AttributeSet attrs) throws javax.swing.text.BadLocationException {
+				StringBuilder sb = new StringBuilder(txtRespuesta.getText());
+				sb.replace(offset, offset + length, text);
+				if (sb.toString().matches("-?\\d*")) {
+					super.replace(fb, offset, length, text, attrs);
+				}
+			}
+		};
+		((AbstractDocument) txtRespuesta.getDocument()).setDocumentFilter(soloNumerosFilter);
+
+		frame.setVisible(true);
+		
+	}
+	
+	public void Check(int respuestaUsuario) {
+		// Suponiendo que aquí se verifica la respuesta
+		boolean esCorrecto = respuestaUsuario == ops.RespuestaCO;
+		Respuesta respuesta = new Respuesta();
+		respuesta.GUICheck(esCorrecto);
+		frame.dispose();
 	}
 }
